b8e1099192d26b8431daf64cc28bea84395c3ad1
kchirag2023 <kumarchirag1804@gmail.com>
2023-09-30 17:49:54 +0530

    Add initial files

---
+++ WEBDEV_assignment/app.js
@@ -0,0 +1,6 @@
+function sure(){
+    location.href="https://accounts.spotify.com/en-GB/login?continue=https%3A%2F%2Fopen.spotify.com%2F"
+}
+function search(){
+    location.href="https://open.spotify.com/search"
+}
